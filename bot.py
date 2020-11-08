d3a4d5cc3676262df7feb77daeaea13091ccc236
full-combo <ruszkowskiolek@gmail.com>
2020-11-08 16:58:22 +0100

    pogczamp

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import datetime
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = ">")
@@ -38,9 +39,22 @@ async def _8ball(ctx, *, question):
 async def staty(ctx):
 	file=open("stat.txt")
 
+
 @client.command()
 async def q(ctx):
 	await ctx.send("#z graspikiem es B)")
 
+cogs = {
+	'cogs.dev', 'cogs.mod'
+}
 
-client.run("NzcwMjcwNTYwMDkyNDIyMTQ0.X5bIXA.pNxSMW8SfadW468EFso8eTIhOTo")
+if __name__ == "__main__":
+	for cog in cogs:
+		client.load_extension(cog)
+		print(f"{cog} has been sucessfully loaded.")
+
+with open("xrckwo.0", "r", encoding="utf-8") as f:
+	bottoken = f.read()
+
+
+client.run(bottoken)
